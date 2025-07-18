979b531c91e946c08382be69e4ece0e124ed6abb
Daksh Jethwa <jethwadaksh7@gmail.com>
2025-07-18 21:42:34 +0530

    fucntions

---
+++ Personal /Project2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+return a + b;
+}
+int Tarea(int a, int b) {
+    return (a*b)/2;
+
+}
+int main() {
+    cout<< sum (5,10)<<endl;
+    cout<< Tarea(5,10)<<endl;
+    return 0;
+}
---
+++ Personal /Project2.h
@@ -0,0 +1,16 @@
+//
+// Created by Daksh Jethwa on 16/07/25.
+//
+
+#ifndef PROJECT2_H
+#define PROJECT2_H
+
+
+
+class Project2 {
+
+};
+
+
+
+#endif //PROJECT2_H
