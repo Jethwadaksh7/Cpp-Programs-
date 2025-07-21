c89c4b7ac399361a1268f3419c07a3d61d08fed3
Daksh Jethwa <jethwadaksh7@gmail.com>
2025-07-21 10:36:03 +0530

    arrays

--- College /Lab Work/lab3.cpp
+++ College /Lab Work/lab3.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
+    int n;
+    int s;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "The elements in the array are: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 
     cout << endl;
     cout << "eneter element to be searched: ";
