418df455fbfe7cd46bd040b6feebee2b152776f8
Daksh Jethwa <jethwadaksh7@gmail.com>
2025-07-21 11:13:48 +0530

    New Loops trial

---
+++ College /Lab Work/lab4.cpp
@@ -0,0 +1,27 @@
+//given a sorted array with duplicate elemnts… find the first and last occurrence of given number.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int min;
+    int total=0;
+    int max;
+    int b;
+    cout<<"Enter the number of elements in the array: ";
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+    cout<<"Enter the number to be searched in the array: ";
+    cin>>a;
+    for(int i=0;i<n;i++) {
+        if(arr[i]==a) {
+            cout<<"element found at position " << i<<endl;
+            total++;
+        }
+    }
+    cout<<"Total number of repearted elements in the array is: "<<total<<endl;
+    }
---
+++ College /Lab Work/lab4.h
@@ -0,0 +1,16 @@
+//
+// Created by Daksh Jethwa on 21/07/25.
+//
+
+#ifndef LAB4_H
+#define LAB4_H
+
+
+
+class lab4 {
+
+};
+
+
+
+#endif //LAB4_H
