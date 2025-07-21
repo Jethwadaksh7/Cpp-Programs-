afb6677f79df42df458f3830b2001908cef2c3b4
Daksh Jethwa <jethwadaksh7@gmail.com>
2025-07-21 10:35:55 +0530

    array

--- College /Lab Work/lab3.cpp
+++ College /Lab Work/lab3.cpp
@@ -3,4 +3,22 @@ using namespace std;
 
 int main() {
 
+    cout << endl;
+    cout << "eneter element to be searched: ";
+    int searched;
+    cin >> searched;
+    bool found = false;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == searched) {
+            found = true;
+            s=i;
+        }
+
+    }
+    if (found == true) {
+        cout <<"element found at position " << s << endl;
+    }
+    else
+        cout << "element not found." << endl;
+    return 0;
 }
